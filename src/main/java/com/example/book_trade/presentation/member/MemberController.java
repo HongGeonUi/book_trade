baf1079d0f823f623b7d7078e4e396c9af4ccfdd
Geonui <keaam12@naver.com>
2023-10-31 16:17:48 +0900

    chore: apply Swagger [#4]

--- src/main/java/com/example/book_trade/presentation/member/MemberController.java
+++ src/main/java/com/example/book_trade/presentation/member/MemberController.java
@@ -2,6 +2,7 @@ package com.example.book_trade.presentation.member;
 
 import com.example.book_trade.application.member.MemberService;
 import com.example.book_trade.presentation.member.dto.MemberDTO;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import jakarta.validation.Valid;
@@ -16,6 +17,7 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    @Operation(summary = "멤버 등록 api", description = "멤버 정보를 등록한다")
     @PostMapping("/api/member")
     public ResponseEntity<?> signup(@Valid @RequestBody MemberDTO req) {
         memberService.signup(req.getEmail(), req.getPassword(), req.getName(), req.getSex());
@@ -24,6 +26,7 @@ public class MemberController {
                 .build();
     }
 
+    @Operation(summary = "멤버 정보 변경 api", description = "멤버 정보를 변경한다.")
     @PutMapping("/api/member")
     public ResponseEntity<?> updateMember(@Valid @RequestBody MemberDTO req) {
         memberService.updateMember(req.getEmail(), req.getPassword(), req.getName(), req.getSex());
@@ -32,6 +35,7 @@ public class MemberController {
                 .build();
     }
 
+    @Operation(summary = "멤버 삭제 api", description = "멤버를 삭제한다.")
     @DeleteMapping("/api/member/{id}")
     public ResponseEntity<?> deleteMember(@PathVariable Long id) {
         memberService.deleteMember(id);
@@ -40,6 +44,7 @@ public class MemberController {
                 .build();
     }
 
+    @Operation(summary = "Id로 멤버 검색 api", description = "Id를 이용해 멤버를 검색한다")
     @GetMapping("/api/member/{id}")
     public ResponseEntity<?> getMemberById(@PathVariable Long id) {
         return ResponseEntity
